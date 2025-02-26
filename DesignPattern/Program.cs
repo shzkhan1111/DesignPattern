625ad871445d657276d8928304699839485b2232
Shahzaib Ahmed Khan <shahzaib.ahmed@hyperdatacomputing.com>
2025-02-26 13:13:34 +0500

    State Dp

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,44 +1,25 @@
-﻿using DesignPattern.DesignPattern;
-using Models;
-using Models.Adapter;
-using Models.builder;
-using Models.ChainOfResposibility;
-using Models.CommandDesignPattern;
-using Models.CompositeDesignPattern;
-using Models.DecoratorDesignPatterb;
-using Models.Enums;
-using Models.Facade;
-using Models.FlyWeight;
-using Models.IteratorDEsignPattern;
-using Models.Memento;
-using Models.PrototypeDesignPattern;
-using Models.Proxy;
-using System.ComponentModel.DataAnnotations;
-using System.Data.Common;
+﻿using Models.StateDesignPattern;
+
+DayOftheYear dayOftheYear = new DayOftheYear();
+bool isMorning = false;
+bool doContinue = true;
 
 
 
+do{
+    dayOftheYear.IsDay = isMorning;
+    if (isMorning)
+    {
+        dayOftheYear.ChangeState(new MorningState());
+    }
+    else
+    {
+        dayOftheYear.ChangeState(new NightState());
+    }
+    dayOftheYear.start();
+    var sel = Console.ReadKey().KeyChar;
+    Console.WriteLine("Press Y to continue");
+    doContinue = sel == 'y' || sel == 'Y';
+    isMorning = !isMorning;
 
-// Create the originator and caretaker
-Models.Memento.TextEditor editor = new Models.Memento.TextEditor();
-History history = new History();
-
-editor.SetText("Hello");
-editor.SetCursorPosition(5);
-editor.SetScrollPosition(0);
-editor.DisplayState();
-//save the history
-history.Push(editor.Save());
-
-editor.SetText("Hello World");
-editor.SetCursorPosition(11);
-editor.SetScrollPosition(0);
-editor.DisplayState();
-history.Push(editor.Save());
-
-editor.Restore(history.Pop());
-editor.DisplayState();
-
-
-editor.Restore(history.Pop());
-editor.DisplayState();
+}while (isMorning);
---
+++ Models/StateDesignPattern/StateDessignPattern.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.StateDesignPattern
+{
+    public interface IWhatWillIDo
+    {
+        public void IWIllDO();
+    }
+    public class MorningState : IWhatWillIDo
+    {
+        public void IWIllDO()
+        {
+            Console.WriteLine("I will Eat Breakfast and Go to sleep");
+        }
+    }
+    public class NightState : IWhatWillIDo
+    {
+        public void IWIllDO()
+        {
+            Console.WriteLine("I will Go to sleep");
+        }
+    }
+
+    public class DayOftheYear
+    {
+        private bool _isDay;
+        private IWhatWillIDo _whatWillIDo;
+        public DayOftheYear(bool isDay = false)
+        {
+            _isDay = isDay;
+        }
+        public DayOftheYear()
+        {
+            _isDay = true;
+        }
+        public bool IsDay 
+        {
+            get { return _isDay; } 
+            set { _isDay = value; } 
+        }
+
+        public void ChangeState(IWhatWillIDo whatWillIDo)
+        {
+            _whatWillIDo = whatWillIDo;
+        }
+        public void start()
+        {
+            _whatWillIDo.IWIllDO();
+        }
+    }
+
+}
